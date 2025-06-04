bc84cba1104b1ca780ad0045fbe32326992799ac
plabsjc <pedro.lucas.sjc@gmail.com>
2025-06-04 00:49:02 -0300

    Tratamento dos dados em adicionar produto, onde a contidade e preço não podem ser == 0

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -9,12 +9,59 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string descricao;
+        double quantidade;
+        double preco;
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
-
+        public string Descricao
+        {
+            get => descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Insira a descrição");
+                }
+                else
+                {
+                    descricao = value;
+                }
+            }
+        }
+        public double Quantidade
+        {
+            get => quantidade;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Insira a quantidade");
+                }
+                else
+                {
+                    quantidade = value;
+                }
+            }
+        }
+        public double Preco
+        {
+            get => preco;
+            set
+            {
+                if(value == 0)
+                {
+                    throw new Exception("Insira o valor corretamente");
+                }
+                else
+                {
+                    preco = value;
+                }
+                
+            }
+        }
         public double Total { get => Quantidade * Preco; }
+
+
     }
 }
